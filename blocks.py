26aa02a2e3a7e77cca61193b90c7ce414efd60bd
francomano <playerhorus@gmail.com>
2023-01-14 00:23:46 +0100

    added attention module

--- blocks.py
+++ blocks.py
@@ -9,12 +9,28 @@ class Self_Attn(nn.Module):
    
     def __init__(self, in_dim):
         super().__init__()
-        
+        self.query_conv = nn.Conv1d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
+        self.key_conv = nn.Conv1d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
+        self.value_conv = nn.Conv1d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
         
         # Initialize gamma as 0
         self.gamma = nn.Parameter(torch.zeros(1))
+        self.softmax=nn.Softmax(dim=-1)
         
     def forward(self,x):
+        
+        
+        query  = self.query_conv(x)
+        key =  self.key_conv(x)
+        energy =  torch.bmm(query.reshape((query.shape[0],query.shape[2],query.shape[1])), key) #Q.T*K
+        
+        attention = self.softmax(energy)
+        proj_value = self.value_conv(x)
+        out = torch.bmm(proj_value, attention) 
+        
+        # Add attention weights onto input
+        out = self.gamma*out + x
+        
         return x
 
 
@@ -25,15 +41,13 @@ class MainBlock(nn.Module):
         layer.append(nn.utils.spectral_norm(nn.Conv1d(in_channels = channels, out_channels=channels,kernel_size=1)))
         layer.append(nn.LeakyReLU())
 
-
-
         self.l = nn.Sequential(*layer)
-        #self.attn = Self_Attn()
+        self.attn = Self_Attn(channels)
         
     
     def forward(self, z):
         out=self.l(z)
-        #out=self.attn(z)
+        out=self.attn(z)
 
         return out
 
@@ -48,7 +62,7 @@ class Generator(nn.Module):
            MainBlock(batch_size)
         ])
         self.n=self.blocks.__len__()
-        
+        self.outlayer=nn.utils.spectral_norm(nn.Conv1d(in_channels =2**(self.n+3), out_channels=2**(self.n+3),kernel_size=1))
         
 
     def forward(self, z):
@@ -62,7 +76,7 @@ class Generator(nn.Module):
         
         z=z.reshape((z.shape[1],z.shape[0],1))
 
-        self.outlayer=nn.utils.spectral_norm(nn.Conv1d(in_channels =2**(self.n+3), out_channels=2**(self.n+3),kernel_size=1))
+        
         z=self.outlayer(z)
         
         return z
