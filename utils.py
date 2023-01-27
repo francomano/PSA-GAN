06da1a42122e3f8e234d550f5235a2cc13992ce4
DavideEspositoPelella <davideexpo00@gmail.com>
2023-01-27 21:58:30 +0100

    minor fix

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import numpy as np
 import torch
+import os
+from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
 
 def sliding_windows(data, seq_length):
     x = []
@@ -29,3 +33,30 @@ def noise(Xt):
     noise = torch.randn((bs, 1, target_len))
     Xt = torch.cat((Xt, noise), dim=1)
     return Xt
+
+
+def create_folder(path):
+    path = path
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+    print("The new models directory is created!")
+    return
+
+
+
+def plot_training_history(name, discriminator_loss, generator_loss):
+  fig, ax1 = plt.subplots(figsize=(20, 6))
+
+  ax1.plot(discriminator_loss, 'o-', label='discriminator loss')
+  ax1.plot(generator_loss, '^-', label='generator loss')
+
+  ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+  ax1.legend()
+  ax1.set_ylabel('Loss')
+  ax1.set_xlabel('Epoch')
+
+  fig.suptitle(f'Training history {name}')
+  print("End-training Generator Loss:",generator_loss[-1])
+  print("End-training Discriminator Loss:",discriminator_loss[-1])
+  return
